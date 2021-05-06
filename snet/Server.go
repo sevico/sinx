72f5444c78d5e7eff503d97bae074b8b47a3db05
swkhack <swkhack@gmail.com>
2021-05-07 00:52:53 +0800

    add server interface

---
+++ snet/Server.go
@@ -0,0 +1,20 @@
+package snet
+
+type Server struct {
+	Name string
+	IPVersion string
+	IP string
+	Port int
+}
+
+func (s *Server) Start() {
+
+}
+
+func (s *Server) Stop() {
+
+}
+
+func (s *Server) Serve() {
+
+}
